8ed46965db11b03ceb9fb7d10ff90c02ba6c459f
Your Name <barneykessel2004@gmail.com>
2022-05-01 14:52:15 +0900

    2nd-commit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import imp
-from turtle import left, right
-from typing import List
+
 import streamlit as st
-import numpy as np 
-import pandas as pd
-from PIL import Image
-import time
 
 st.title('Streamlit 超入門')
 
